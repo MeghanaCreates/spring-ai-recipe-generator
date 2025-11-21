56c56df1fe8e088e226f1534198586d56885b092
MeghanaGadwal <grmeghana14@gmail.com>
2025-11-21 21:15:47 +0100

    Update WebConfig.java

--- src/main/java/com/ai/SpringAIProject/WebConfig.java
+++ src/main/java/com/ai/SpringAIProject/WebConfig.java
@@ -14,7 +14,8 @@ public class WebConfig implements WebMvcConfigurer{
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
 				registry.addMapping("/**")
-				        .allowedOrigins("http://localhost:3000/")
+				        .allowedOrigins( "http://localhost:3000",
+                "https://spring-ai-recipe-generator-react.vercel.app")
 				        .allowedMethods("GET","POST","PUT","DELETE","OPTIONS")
 				        .allowedHeaders("*")
 				        .allowCredentials(true);
@@ -24,3 +25,4 @@ public class WebConfig implements WebMvcConfigurer{
 	}
 
 }
+
